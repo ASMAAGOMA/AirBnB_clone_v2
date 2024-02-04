eba665b202fa2b521294a43d7d28db2994cd1787
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-04 12:10:34 +0200

    :"(

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,10 @@
 --database hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 --create user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+--give select privilleges
+GRANT SELECT ON performance_schema.* to 'hbnb_test'@'localhost';
+FLUSH PRIVELLEGES;
+--give all privilleges
+GRANT ALL PRIVELLEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVELLEGES;
