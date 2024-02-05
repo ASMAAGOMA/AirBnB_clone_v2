abfbdc56c5aff422e66dd501273214f9b4b0804b
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-05 09:32:25 +0200

    :"(

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,10 @@
 --database hbnb_test_db
-CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 --create user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 --give select privilleges
-GRANT SELECT ON performance_schema.* to 'hbnb_test'@'localhost';
-FLUSH PRIVELLEGES;
+GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
 --give all privilleges
-GRANT ALL PRIVELLEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVELLEGES;
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 --give select privilleges
 GRANT SELECT ON performance_schema.* to 'hbnb_test'@'localhost';
-FLUSH PRIVELLEGES;
+FLUSH PRIVILEGES;
 --give all privilleges
-GRANT ALL PRIVELLEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVELLEGES;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
