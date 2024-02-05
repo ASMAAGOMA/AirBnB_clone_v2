72ff3777ab00ffdde81c4b846aa82254f0dde2bc
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-06 01:23:54 +0200

    :"(

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
+import uuid
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DATETIME
 from models.base_model import BaseModel
-
+from sqlalchemy.orm import relationship
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
---
+++ models/engine/db_storage.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""db storage"""
+
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from os import getenv
+
+classes = {"User": User, "Place": Place, "Amenity": Amenity, "State": State
+        , "City": City, "Review": Review, }
+
+class DBStorage:
+    """database storage"""
+    __engine = None
+    __session = None
+    
+    def __init__(self):
+        """engine"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                        .format(HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB), pool_pre_ping=True)
+        if HBNB_ENV == 'test':
+            BaseModel.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        Returns the list of objects of one type of class.
+        If cls is None, returns all objects.
+        """
+        dicty = {}
+        if cls is None:
+            for i in classes.values():
+                objects = self.__session.query(i).all()
+                for object in objects:
+                    the_key = object.__class__.__name__ + '.' + object.id
+                    dicty = object[the_key]
+        else:
+            objects = self.__session.query(cls).all()
+            for object in objects:
+                the_key = object.__class__.__name__ + '.' + object.id
+                dicty = object[the_key]
+        return dicty
+
+    def new(self, obj):
+        """adds a new object"""
+        if obj != None:
+            self.__session.add(obj)
+            self.__session.flush()
+            self.__session.refresh()
+
+    def save(self):
+        """commits changes"""
+        self.__session.commit()
