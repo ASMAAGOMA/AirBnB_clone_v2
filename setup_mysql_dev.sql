761e51a9c8e57fa8510d023826ba7759e480b4c2
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-05 09:46:41 +0200

    :"(

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,34 @@
---database hbnb_test_db
+-- Check if the database exists
+SELECT IF(
+    EXISTS (
+        SELECT SCHEMA_NAME
+        FROM INFORMATION_SCHEMA.SCHEMATA
+        WHERE SCHEMA_NAME = 'hbnb_dev_db'
+    ),
+    'Database exists',
+    'Database does not exist'
+);
+
+-- Check if the user exists
+SELECT IF(
+    EXISTS (
+        SELECT USER
+        FROM mysql.user
+        WHERE USER = 'hbnb_dev' AND HOST = 'localhost'
+    ),
+    'User exists',
+    'User does not exist'
+);
+
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
---create user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
---give select privilleges
-GRANT SELECT ON performance_schema.* to 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
---give all privilleges
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant privileges to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges
 FLUSH PRIVILEGES;
