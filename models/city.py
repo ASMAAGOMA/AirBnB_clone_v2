b07e7913bb46b22532e20f43d9219f46ecb0d350
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-06 01:45:44 +0200

    :"(

--- models/city.py
+++ models/city.py
@@ -3,10 +3,18 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, DATETIME
-from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, DATETIME, ForeignKey
+from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
+from models import store_type
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
+    if store_type == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+        places = relationship('Place', backref='cities', cascade='all, delete, delete-orphan')
+    else:
+        name = ''
+        state_id = ''
