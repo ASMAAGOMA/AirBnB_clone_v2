be54dce7114187ac9e569c82af69574eedab8970
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-05 09:50:48 +0200

    :"(

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,25 +1,3 @@
--- Check if the database exists
-SELECT IF(
-    EXISTS (
-        SELECT SCHEMA_NAME
-        FROM INFORMATION_SCHEMA.SCHEMATA
-        WHERE SCHEMA_NAME = 'hbnb_dev_db'
-    ),
-    'Database exists',
-    'Database does not exist'
-);
-
--- Check if the user exists
-SELECT IF(
-    EXISTS (
-        SELECT USER
-        FROM mysql.user
-        WHERE USER = 'hbnb_dev' AND HOST = 'localhost'
-    ),
-    'User exists',
-    'User does not exist'
-);
-
 -- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
