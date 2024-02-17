2cf87d19e2db013e64f258fb918b3fbd06e8c808
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-17 21:56:31 +0200

    :"(

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -42,7 +42,7 @@ def num(n):
 @flask_app.route("/number_template/<int:n>", strict_slashes=False)
 def num(n):
     """html"""
-    return render_template("5-number.html", n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
