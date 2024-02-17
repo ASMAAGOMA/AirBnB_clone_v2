37a065f141daa5eb870857f69f8aef3274dda607
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-17 19:13:17 +0200

    :"(

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,13 @@
 
 from flask import Flask
 flask_app = Flask(__name__)
+
+
 @flask_app.route("/", strict_slashes=False)
 def homePage():
     """Hello HBNB"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     flask_app.run(host='0.0.0.0', port=5000)
