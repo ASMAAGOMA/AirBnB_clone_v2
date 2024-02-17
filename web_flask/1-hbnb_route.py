72a884ec6a7a0334698d39621042f8f257727d44
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-17 19:35:33 +0200

    :"(

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+flask_app = Flask(__name__)
+
+
+@flask_app.route("/", strict_slashes=False)
+def homePage():
+    """Hello HBNB"""
+    return "Hello HBNB!"
+
+
+flask_app.route("/hbnb", strict_slashes=False)
+
+
+def homePage():
+    """HBNB"""
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    flask_app.run(host='0.0.0.0', port=5000)
