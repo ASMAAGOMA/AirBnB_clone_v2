b3a8925301b8f9af1393ac9a126578502343b121
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-17 19:06:55 +0200

    h

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
+from flask import Flask
+flask_app = Flask(__name__)
+@flask_app.route("/", strict_slashes=False)
+def homePage():
+    """Hello HBNB"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    flask_app.run(host='0.0.0.0', port=5000)
