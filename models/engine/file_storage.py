b11b6a97031a622bb542d97e831208bcb2e9cd2c
ASMAAGOMA <asmaagadallaag@gmail.com>
2024-02-18 17:59:07 +0200

    :"(

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,42 +1,45 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""file storage"""
 import json
-import os
+
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = "file.json"
+    """file storage"""
+    __file_path = 'file.json'
     __objects = {}
 
+    def close(self):
+        """Close"""
+        self.reload()
+
     def all(self, cls=None):
-        """
-        Returns the list of objects of one type of class.
-        If cls is None, returns all objects.
-        """
+        """Returns a dictionary"""
         if cls is None:
             return self.__objects
-        name = cls.__name__
-        dicty = {}
+        cls_name = cls.__name__
+        dct = {}
         for key in self.__objects.keys():
-            if key.split('.')[0] == name:
-                dicty[key] = self.__objects[key]
-        return dicty
+            if key.split('.')[0] == cls_name:
+                dct[key] = self.__objects[key]
+        return dct
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """Add"""
+        self.__objects.update(
+            {obj.to_dict()['__class__'] + '.' + obj.id: obj}
+            )
 
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        """Save"""
+        with open(self.__file_path, 'w') as f:
             temp = {}
-            temp.update(FileStorage.__objects)
+            temp.update(self.__objects)
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load"""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -46,31 +49,27 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
-
+                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
+                    'State': State, 'City': City, 'Amenity': Amenity,
+                    'Review': Review
+                  }
         try:
-            with open(FileStorage.__file_path, encoding="utf-8") as json_file:
-                data = json.load(json_file)
-                for key, val in data.items():
-                    class_name = val.pop('__class__', None)
-                    if class_name in classes:
-                        obj = classes[class_name](**val)
-                        self.__objects[key] = obj  # Update the dictionary with the new object
-                    else:
-                        print(f"Unknown class: {class_name}")
+            temp = {}
+            with open(self.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
-        """delete"""
-        if obj == None:
+        ''' deletes
+        '''
+        if obj is None:
             return
-        the_key = obj.to_dict()['__class__'] + '.' + obj.id
-        if the_key in self.__objects.keys():
-            del self.__objects[the_key]
+        obj_key = obj.to_dict()['__class__'] + '.' + obj.id
+        if obj_key in self.__objects.keys():
+            del self.__objects[obj_key]
 
     def close(self):
         """close"""
